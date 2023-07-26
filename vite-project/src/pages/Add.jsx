429ec67f1fe7d6b2f2aaf56336cdf4027bf0da8e
langochai <langochai11121998@gmail.com>
2023-07-26 10:47:11 +0700

    update

--- vite-project/src/pages/Add.jsx
+++ vite-project/src/pages/Add.jsx
@@ -30,7 +30,7 @@ export const Add = () => {
             <p>Price:</p>
             <input type="text" name="price" onChange={handleChange}/>
             <p>Description:</p>
-            <textarea name="description" onChange={handleChange} style={{widt:"80vw"}}/>
+            <textarea name="description" onChange={handleChange} style={{width:"80vw"}}/>
             <br/>
             <Button variant="contained" onClick={handleClick}>Add</Button>
             <Link to="/">
